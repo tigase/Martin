00c6c94de19f572943500b67159ea4659bd3bf96
Wojtek <wojtek@unir.se>
2022-06-10 10:48:58 +0200

    fix parent pom version and licence headers

--- Documentation/generate.sh
+++ Documentation/generate.sh
@@ -1,3 +1,10 @@
+#
+# Tigase Swift Library - Documentation - bootstrap configuration for all Tigase projects
+# Copyright (C) 2004 Tigase, Inc. (office@tigase.com) - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+
 mkdir target
 asciidoctor -D target asciidoc/index.asciidoc
 asciidoctor-pdf -D target asciidoc/index.asciidoc
